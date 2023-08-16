206d622aab32a0475b7873f7353991e426609c2d
윤지유 <132847297+yoonjiyoo@users.noreply.github.com>
2023-08-16 20:23:19 +0900

    230816 yoonjiyoo_2

--- 20230816/YOONJIYOO/Algorithm_NM.py
+++ 20230816/YOONJIYOO/Algorithm_NM.py
@@ -1,13 +1,19 @@
 """
-새로 생긴 놀이기구는 인기가 매우 많아 줄이 끊이질 않습니다. 
-이 놀이기구의 원래 이용료는 price원 인데, 놀이기구를 N 번 째 이용한다면 원래 이용료의 N배를 받기로 하였습니다. 
-즉, 처음 이용료가 100이었다면 2번째에는 200, 3번째에는 300으로 요금이 인상됩니다.
-놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return 하도록 solution 함수를 완성하세요.
-단, 금액이 부족하지 않으면 0을 return 하세요.
+ 
+*** if (price)^count > money then return==(price)^count-money, else return==0
+
 """
 
 
 def solution(price, money, count):
-    answer = -1
+    answer = 0
+
+    for i in range(1, count + 1):
+        answer += price * i  # 여기서 반복문 실수... 확인하기~
+
+    if answer > money:
+        answer = answer - money
+    else:
+        answer = 0
 
     return answer
