84ef1c7e5dfe160bf06c788283a82e9e4c355dc2
jhyungit <jh021199@gmail.com>
2023-08-21 13:44:52 +0900

    jh_commit

---
+++ 20230823/JeonghyunLEE/Algorithm_CH.py
@@ -0,0 +1,20 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42888
+
+def solution(record):
+    answer = []
+    u_dict = dict()
+    
+    for r in record:
+        r = r.split(" ")
+        if r[0] == 'Enter':
+            u_dict[r[1]] = r[2] # 마지막 입장한 id 저장
+        elif r[0] == 'Change':
+            u_dict[r[1]] = r[2] # 바뀐 id로 저장
+    
+    for r in record:
+        r = r.split(" ")
+        if r[0] == 'Enter':
+            answer.append("{0}님이 들어왔습니다.".format(u_dict[r[1]]))
+        elif r[0] == 'Leave':
+            answer.append("{0}님이 나갔습니다.".format(u_dict[r[1]]))
+    return answer
