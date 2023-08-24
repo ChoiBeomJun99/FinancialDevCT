a2e562abb228d019fc485e11b4567c3573d9e891
jhyungit <jh021199@gmail.com>
2023-08-24 15:17:55 +0900

    jh_complete

---
+++ 20230826/JeonghyunLEE/Algorithm_NM.py
@@ -0,0 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/131705
+
+from itertools import combinations
+
+def solution(number):
+    answer = 0
+    for i in combinations(number,3):
+        if sum(i) == 0:
+            answer += 1
+    return answer
