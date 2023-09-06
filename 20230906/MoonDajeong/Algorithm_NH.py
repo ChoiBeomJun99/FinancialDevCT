8b4bb2a29a67862f996ecdf4602e40a05b87be20
문다정 <mioke0509@naver.com>
2023-09-06 15:23:27 +0900

    0906다정

---
+++ 20230906/MoonDajeong/Algorithm_NH.py
@@ -0,0 +1,16 @@
+#알고리즘(NM)_시저암호
+
+#쫌더고민해볼게여여..
+#%%
+# for i in range(0,len(s)):
+#     word = s[i]
+#     newword = ''
+#     #공백먼저
+#     if word = ' ':
+#         newword += ' '
+#     #대문자인경우
+#     elif (ord(word))<91:
+#         if()
+#     #소문자인경우
+#     elif (ord(word))>96:
+#         if()
