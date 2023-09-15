9c88bca08b1cd59afef72dcd711bf7b9694d02ed
jhyungit <jh021199@gmail.com>
2023-09-15 22:04:30 +0900

    complete

--- 20230913/JeonghyunLEE/Algorithm_NM.py
+++ 20230913/JeonghyunLEE/Algorithm_NM.py
@@ -1,11 +1,11 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/12926
+# https://school.programmers.co.kr/learn/courses/30/lessons/12950
 
-def solution(s, n):
-    s = list(s)
-
-    for i in range(len(s)):
-        if s[i].isupper():
-            s[i] = chr((ord(s[i]) - ord('A') + n) % 26 + ord('A'))
-        elif s[i].islower():
-             s[i] = chr((ord(s[i]) - ord('a') + n) % 26 + ord('a'))
-    return "".join(s)
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        answer.append([])
+        
+    for i in range(len(arr1)):
+        for j in range(len(arr1[i])):
+            answer[i].append(arr1[i][j] + arr2[i][j])
+    return answer
